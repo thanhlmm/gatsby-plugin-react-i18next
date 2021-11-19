fc218943b2740ab422c7e1f61c86e8fd6286abc1
Thanh Le <mihthanh27@gmail.com>
2021-11-19 22:26:15 +0700

    Add build version

---
+++ dist/plugin/onPreBootstrap.d.ts
@@ -0,0 +1,6 @@
+import {ParentSpanPluginArgs} from 'gatsby';
+import {PluginOptions} from '../types';
+export declare const onPreBootstrap: (
+  _args: ParentSpanPluginArgs,
+  pluginOptions: PluginOptions
+) => void;
---
+++ dist/plugin/wrapPageElement.d.ts
@@ -0,0 +1,12 @@
+import {WrapPageElementBrowserArgs} from 'gatsby';
+import {PageContext, PluginOptions} from '../types';
+export declare const wrapPageElement: (
+  {element, props}: WrapPageElementBrowserArgs<any, PageContext>,
+  {
+    i18nextOptions,
+    redirect,
+    generateDefaultLanguagePage,
+    siteUrl,
+    localeJsonNodeName
+  }: PluginOptions
+) => JSX.Element | null | undefined;
